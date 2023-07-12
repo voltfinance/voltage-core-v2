e16e64d641f21e88cc0f5355261c45ff23d109d7
mul53 <mulengabowa53@gmail.com>
2023-07-12 22:03:53 +0200

    add reward distributor

--- contracts/misc/RewardDistributor.sol
+++ contracts/misc/RewardDistributor.sol
@@ -1,27 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract RewardDistributor is Ownable, ReentrancyGuard {
+contract RewardDistributor is ReentrancyGuard {
     struct Reward {
-        uint256 rewardId;
-        address rewardToken;
-        uint256 rewardAmount;
+        uint256 id;
+        string title;
+        string description;
+        address token;
+        uint256 amount;
         bool isActive;
+        bool isVariableReward;
+        bool isNative;
     }
 
     struct Distribution {
         address user;
         uint256 rewardId;
+        address rewardToken;
         uint256 rewardAmount;
         bool isClaimed;
         bool isActive;
     }
 
     uint256 public totalRewardCount;
+
+    address public owner;
+
+    address public distributor;
 
     mapping(uint256 => Reward) public rewards;
 
@@ -30,15 +38,52 @@ contract RewardDistributor is Ownable, ReentrancyGuard {
 
     mapping(address => Distribution[]) public userDistributions;
 
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier onlyDistributor() {
+        require(distributor == msg.sender);
+        _;
+    }
+
+    constructor(address _owner, address _distributor) {
+        owner = _owner;
+        distributor = _distributor;
+    }
+
+    function getUserDistributions(
+        address _user
+    ) public view returns (Distribution[] memory) {
+        return userDistributions[_user];
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+
+    function changeDistributor(address _newDistributor) public onlyOwner {
+        distributor = _newDistributor;
+    }
+
     function addReward(
-        address _rewardToken,
-        uint256 _rewardAmount
+        string memory _title,
+        string memory _description,
+        address _token,
+        uint256 _amount,
+        bool _isVariableReward,
+        bool _isNative
     ) public onlyOwner {
         Reward memory _reward = Reward({
-            rewardId: totalRewardCount,
-            rewardToken: _rewardToken,
-            rewardAmount: _rewardAmount,
-            isActive: true
+            id: totalRewardCount,
+            title: _title,
+            description: _description,
+            token: _token,
+            amount: _amount,
+            isActive: true,
+            isVariableReward: _isVariableReward,
+            isNative: _isNative
         });
 
         rewards[totalRewardCount] = _reward;
@@ -48,20 +93,33 @@ contract RewardDistributor is Ownable, ReentrancyGuard {
 
     function addDistribution(
         uint256 _rewardId,
-        address _user
-    ) public onlyOwner {
+        address _user,
+        uint256 _amount
+    ) public onlyDistributor {
         Reward memory reward = rewards[_rewardId];
 
-        require(!reward.isActive, "addDistribution: reward not found");
+        require(reward.isActive, "addDistribution: reward not found");
         require(
             !userDistribution[_rewardId][_user].isActive,
             "addDistribution: distribution already added"
         );
 
+        if (reward.isVariableReward) {
+            require(
+                _amount > 0,
+                "addDistribution: amount should be greater than 0"
+            );
+        }
+
+        uint256 rewardAmount = reward.isVariableReward
+            ? _amount
+            : reward.amount;
+
         Distribution memory distribution = Distribution({
             user: _user,
             rewardId: _rewardId,
-            rewardAmount: reward.rewardAmount,
+            rewardToken: reward.token,
+            rewardAmount: rewardAmount,
             isClaimed: false,
             isActive: true
         });
@@ -89,13 +147,29 @@ contract RewardDistributor is Ownable, ReentrancyGuard {
 
         distribution.isClaimed = true;
 
-        IERC20(reward.rewardToken).transfer(msg.sender, reward.rewardAmount);
+        if (reward.isNative) {
+            address payable _user = payable(distribution.user);
+            (bool sent, ) = _user.call{value: distribution.rewardAmount}("");
+            require(sent, "Failed to send FUSE");
+        } else {
+            IERC20(reward.token).transfer(
+                msg.sender,
+                distribution.rewardAmount
+            );
+        }
     }
 
     function withdrawRewardToken(address _rewardToken) public onlyOwner {
         IERC20(_rewardToken).transfer(
-            owner(),
+            owner,
             IERC20(_rewardToken).balanceOf(address(this))
         );
     }
+
+    function withdrawFUSE() public onlyOwner {
+        (bool sent, ) = owner.call{value: address(this).balance}("");
+        require(sent, "Failed to send FUSE");
+    }
+
+    receive() external payable {}
 }
---
+++ test/misc/RewardDistributor.test.ts
@@ -0,0 +1,209 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+import { RewardDistributor, TestERC20 } from '../../typechain-types'
+import { formatEther } from 'ethers/lib/utils'
+
+describe('RewardDistributor', () => {
+  let rewardDistributor: RewardDistributor
+  let owner: SignerWithAddress
+  let distributor: SignerWithAddress
+  let user: SignerWithAddress
+  let token: TestERC20
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners()
+    owner = signers[0]
+    distributor = signers[1]
+    user = signers[2]
+
+    const RewardDistributorFactory = await ethers.getContractFactory(
+      'RewardDistributor',
+    )
+    rewardDistributor = await RewardDistributorFactory.deploy(
+      owner.address,
+      distributor.address,
+    )
+
+    const TestERC20Factory = await ethers.getContractFactory('TestERC20')
+    token = await TestERC20Factory.deploy(
+      'Volt Token',
+      'VOLT',
+      18,
+      ethers.utils.parseEther('100000'),
+    )
+
+    await token.transfer(
+      rewardDistributor.address,
+      ethers.utils.parseEther('100000'),
+    )
+    await owner.sendTransaction({
+      to: rewardDistributor.address,
+      value: ethers.utils.parseEther('500'),
+    })
+  })
+
+  describe('add reward', () => {
+    it('owner should be able to add fuse reward', async () => {
+      await expect(
+        rewardDistributor.addReward(
+          '50 Volt',
+          'Welcome Reward',
+          token.address,
+          ethers.utils.parseEther('50'),
+          false,
+          true,
+        ),
+      ).not.reverted
+    })
+
+    it('owner should be able to add token reward', async () => {
+      await expect(
+        rewardDistributor.addReward(
+          '50 Volt',
+          'Welcome Reward',
+          token.address,
+          ethers.utils.parseEther('50'),
+          false,
+          false,
+        ),
+      ).not.reverted
+    })
+  })
+
+  describe('add distribution', () => {
+    it('distributor should be able to add variable distribution', async () => {
+      await rewardDistributor.addReward(
+        '50 Volt',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        true,
+        false,
+      )
+
+      await expect(
+        rewardDistributor
+          .connect(distributor)
+          .addDistribution(0, user.address, ethers.utils.parseEther('50')),
+      ).not.reverted
+    })
+    it('distributor should be able to add fixed distribution', async () => {
+      await rewardDistributor.addReward(
+        '50 Volt',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        false,
+        false,
+      )
+
+      await expect(
+        rewardDistributor
+          .connect(distributor)
+          .addDistribution(0, user.address, ethers.utils.parseEther('50')),
+      ).not.reverted
+    })
+  })
+
+  describe('claim distribution', () => {
+    it('user should be able to claim token variable distribution', async () => {
+      rewardDistributor.addReward(
+        'Deposit Volt Reward',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        true,
+        false,
+      )
+
+      await rewardDistributor
+        .connect(distributor)
+        .addDistribution(0, user.address, ethers.utils.parseEther('75'))
+
+      await rewardDistributor.connect(user).claimDistribution(0)
+
+      expect(await token.balanceOf(user.address)).to.equal(
+        ethers.utils.parseEther('75'),
+      )
+    })
+    it('user should be able to claim token fixed distribution', async () => {
+      await rewardDistributor.addReward(
+        '50 Volt',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        false,
+        false,
+      )
+
+      await rewardDistributor
+        .connect(distributor)
+        .addDistribution(0, user.address, 0)
+
+      await rewardDistributor.connect(user).claimDistribution(0)
+
+      expect(await token.balanceOf(user.address)).to.equal(
+        ethers.utils.parseEther('50'),
+      )
+    })
+    it('user should be able to claim fuse variable distribution', async () => {
+      rewardDistributor.addReward(
+        'Deposit Volt Reward',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        true,
+        true,
+      )
+
+      await rewardDistributor
+        .connect(distributor)
+        .addDistribution(0, user.address, ethers.utils.parseEther('75'))
+
+      const balanceBefore = await user.getBalance()
+
+      await rewardDistributor.connect(user).claimDistribution(0)
+
+      expect(await user.getBalance()).to.within(
+        balanceBefore.add(ethers.utils.parseEther('74.99')),
+        balanceBefore.add(ethers.utils.parseEther('75')),
+      )
+    })
+    it('user should be able to claim fuse fixed distribution', async () => {
+      rewardDistributor.addReward(
+        'Deposit Volt Reward',
+        'Welcome Reward',
+        token.address,
+        ethers.utils.parseEther('50'),
+        false,
+        true,
+      )
+
+      await rewardDistributor
+        .connect(distributor)
+        .addDistribution(0, user.address, ethers.utils.parseEther('50'))
+
+      const balanceBefore = await user.getBalance()
+
+      await rewardDistributor.connect(user).claimDistribution(0)
+
+      expect(await user.getBalance()).to.within(
+        balanceBefore.add(ethers.utils.parseEther('49.99')),
+        balanceBefore.add(ethers.utils.parseEther('50')),
+      )
+    })
+  })
+
+  it('should withdraw all fuse', async () => {
+    expect(
+      await ethers.provider.getBalance(rewardDistributor.address),
+    ).to.equal('500000000000000000000')
+
+    await rewardDistributor.withdrawFUSE()
+
+    expect(
+      await ethers.provider.getBalance(rewardDistributor.address),
+    ).to.equal('0')
+  })
+})
